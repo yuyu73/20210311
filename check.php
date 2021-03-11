8956f68f24fe0037ee5c320ab842d116a6e59500
yuyu73 <79617568+yuyu73@users.noreply.github.com>
2021-03-11 11:57:32 +0800

    Add files via upload

---
+++ check.php
@@ -0,0 +1,11 @@
+<?php
+echo $_POST["degree"] . "<br>";
+// if($_POST["e-mail"]=="root" && $_POST["password"]=="123")
+// {
+//     header("Location: https://www.nfu.edu.tw/zh/");
+// }
+// else
+// {                 
+//     header("Location: index.html");
+// }
+?>
